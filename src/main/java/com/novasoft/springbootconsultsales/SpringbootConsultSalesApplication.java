0f91a5a7891cbd44e83740b0b7211b360c6fccf4
abelmiraval <abel.miraval.rp@viaexperis2.pe>
2024-07-26 14:20:03 -0500

    First commit

---
+++ src/main/java/com/novasoft/springbootconsultsales/SpringbootConsultSalesApplication.java
@@ -0,0 +1,13 @@
+package com.novasoft.springbootconsultsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConsultSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConsultSalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/novasoft/springbootconsultsales/SpringbootConsultSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novasoft.springbootconsultsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConsultSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
